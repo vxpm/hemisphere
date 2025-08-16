045766eaca1db56a8980791b52560b89976dc627
vinícius x <viniciusximenespm@gmail.com>
2025-08-16 14:21:27 -0300

    wip

---
+++ hemisphere/src/bus.rs
@@ -0,0 +1,25 @@
+use crate::mmu::{Memory, RAM_LEN};
+use hemicore::{Address, Primitive};
+
+#[derive(Default)]
+struct Bus {
+    mem: Memory,
+}
+
+impl Bus {
+    /// Reads a primitive from the given physical address.
+    pub fn read<P: Primitive>(&self, addr: Address) -> P {
+        match addr.value() {
+            0..RAM_LEN => P::read_from_buf(&*self.mem.ram),
+            _ => panic!(),
+        }
+    }
+
+    /// Writes a primitive to the given physical address.
+    pub fn write<P: Primitive>(&mut self, addr: Address, value: P) {
+        match addr.value() {
+            0..RAM_LEN => value.write_to_buf(&mut *self.mem.ram),
+            _ => panic!(),
+        }
+    }
+}
--- hemisphere/src/lib.rs
+++ hemisphere/src/lib.rs
@@ -1,30 +1,5 @@
+pub mod bus;
 pub mod mmu;
-
-use crate::mmu::{Memory, RAM_LEN};
-use hemicore::{Address, Primitive};
-
-#[derive(Default)]
-struct Bus {
-    mem: Memory,
-}
-
-impl Bus {
-    /// Reads a primitive from the given physical address.
-    pub fn read<P: Primitive>(&self, addr: Address) -> P {
-        match addr.value() {
-            0..RAM_LEN => P::read_from_buf(&*self.mem.ram),
-            _ => panic!(),
-        }
-    }
-
-    /// Writes a primitive to the given physical address.
-    pub fn write<P: Primitive>(&mut self, addr: Address, value: P) {
-        match addr.value() {
-            0..RAM_LEN => value.write_to_buf(&mut *self.mem.ram),
-            _ => panic!(),
-        }
-    }
-}
 
 #[cfg(test)]
 mod test {
