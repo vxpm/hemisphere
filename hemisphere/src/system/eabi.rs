a49631130009e7b9f6ae28fff1562293ac8be1d1
vinícius x <viniciusximenespm@gmail.com>
2025-09-19 13:07:24 -0300

    make callstack code prettier

--- hemisphere/src/system/eabi.rs
+++ hemisphere/src/system/eabi.rs
@@ -39,47 +39,41 @@ impl std::fmt::Display for CallStack {
 impl System {
     pub fn call_stack(&self) -> CallStack {
         let mut call_stack = Vec::new();
-        let mut current_sp = self.cpu.user.gpr[1];
-        let mut current_lr = self.cpu.pc.value();
+        let mut current_frame = self.cpu.user.gpr[1];
+        let mut current_routine = self.cpu.user.lr;
 
         loop {
-            let Some(prev_addr) = self.translate_data_addr(Address(current_sp)) else {
-                break;
-            };
-
-            let Some(prev) = self.bus.read_pure(prev_addr) else {
-                break;
-            };
-
-            let Some(link_addr) = self.translate_data_addr(Address(current_sp.wrapping_add(4)))
-            else {
-                break;
-            };
-
-            let Some(link) = self.bus.read_pure(link_addr) else {
-                break;
-            };
-
-            let routine = self
-                .config
-                .executable
-                .as_ref()
-                .and_then(|e| e.find_symbol(Address(current_lr)))
-                .map(|s| s.into_owned());
-
-            call_stack.push(StackFrame {
-                routine,
-                address: Address(current_sp),
-                previous: Address(prev),
-                link: Address(link),
-            });
-
-            if prev == current_sp || prev == 0 {
+            if current_frame == 0 || current_routine == 0 {
                 break;
             }
 
-            current_sp = prev;
-            current_lr = link;
+            let prev_frame_addr = Address(current_frame);
+            let prev_routine_addr = Address(current_frame.wrapping_add(4));
+
+            if let Some(prev_frame_addr) = self.translate_data_addr(prev_frame_addr)
+                && let Some(prev_routine_addr) = self.translate_data_addr(prev_routine_addr)
+                && let Some(prev_frame) = self.bus.read_pure(prev_frame_addr)
+                && let Some(prev_routine) = self.bus.read_pure(prev_routine_addr)
+            {
+                let routine = self
+                    .config
+                    .executable
+                    .as_ref()
+                    .and_then(|e| e.find_symbol(Address(current_routine)))
+                    .map(|s| s.into_owned());
+
+                call_stack.push(StackFrame {
+                    routine,
+                    address: Address(current_frame),
+                    previous: Address(prev_frame),
+                    link: Address(prev_routine),
+                });
+
+                current_frame = prev_frame;
+                current_routine = prev_routine;
+            } else {
+                break;
+            }
         }
 
         CallStack(call_stack)
