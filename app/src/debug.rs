fd44c1a61017eb02a73b885e1173f4777774e570
vinícius x <viniciusximenespm@gmail.com>
2025-09-26 14:47:20 -0300

    debug

--- app/src/debug.rs
+++ app/src/debug.rs
@@ -15,61 +15,58 @@ impl WindowUi for Window {
 
     fn show(&mut self, ui: &mut egui::Ui, ctx: &mut Ctx, state: &mut State) {
         let call_stack = state.hemisphere().system.call_stack();
-        egui::ScrollArea::vertical()
-            .auto_shrink(false)
-            .show(ui, |ui| {
-                let builder = TableBuilder::new(ui)
-                    .auto_shrink(egui::Vec2b::new(false, true))
-                    .striped(true)
-                    .resizable(false)
-                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                    .column(Column::auto()) // addr
-                    .column(Column::auto()) // stack
-                    .column(Column::remainder().at_least(200.0)); // symbol
+        egui::ScrollArea::both().auto_shrink(false).show(ui, |ui| {
+            let builder = TableBuilder::new(ui)
+                .auto_shrink(egui::Vec2b::new(false, true))
+                .striped(true)
+                .resizable(false)
+                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                .column(Column::auto()) // addr
+                .column(Column::auto()) // stack
+                .column(Column::remainder().at_least(200.0)); // symbol
 
-                let table = builder.header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.label("Address");
-                    });
-                    header.col(|ui| {
-                        ui.label("Stack");
-                    });
-                    header.col(|ui| {
-                        ui.label("Symbol");
-                    });
+            let table = builder.header(20.0, |mut header| {
+                header.col(|ui| {
+                    ui.label("Address");
                 });
-
-                table.body(|mut body| {
-                    for call in call_stack.0 {
-                        body.row(20.0, |mut row| {
-                            row.col(|ui| {
-                                let text = egui::RichText::new(call.address.to_string())
-                                    .family(egui::FontFamily::Monospace)
-                                    .color(Color32::LIGHT_BLUE);
-
-                                ui.label(text);
-                            });
-
-                            row.col(|ui| {
-                                let text = egui::RichText::new(call.stack.to_string())
-                                    .family(egui::FontFamily::Monospace)
-                                    .color(Color32::LIGHT_GREEN);
-
-                                ui.label(text);
-                            });
-
-                            row.col(|ui| {
-                                let text = egui::RichText::new(
-                                    call.symbol.as_deref().unwrap_or("<unknown>"),
-                                )
-                                .family(egui::FontFamily::Monospace)
-                                .color(Color32::GRAY);
-
-                                ui.label(text);
-                            });
-                        })
-                    }
+                header.col(|ui| {
+                    ui.label("Stack");
+                });
+                header.col(|ui| {
+                    ui.label("Symbol");
                 });
             });
+
+            table.body(|mut body| {
+                for call in call_stack.0 {
+                    body.row(20.0, |mut row| {
+                        row.col(|ui| {
+                            let text = egui::RichText::new(call.address.to_string())
+                                .family(egui::FontFamily::Monospace)
+                                .color(Color32::LIGHT_BLUE);
+
+                            ui.label(text);
+                        });
+
+                        row.col(|ui| {
+                            let text = egui::RichText::new(call.stack.to_string())
+                                .family(egui::FontFamily::Monospace)
+                                .color(Color32::LIGHT_GREEN);
+
+                            ui.label(text);
+                        });
+
+                        row.col(|ui| {
+                            let text =
+                                egui::RichText::new(call.symbol.as_deref().unwrap_or("<unknown>"))
+                                    .family(egui::FontFamily::Monospace)
+                                    .color(Color32::GRAY);
+
+                            ui.label(text);
+                        });
+                    })
+                }
+            });
+        });
     }
 }
