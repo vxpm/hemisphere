571fc1e6875268785acd9f6988fe69886f4449f8
vinícius x <viniciusximenespm@gmail.com>
2025-10-14 01:28:35 -0300

    fix exceptions

--- hemisphere/src/lib.rs
+++ hemisphere/src/lib.rs
@@ -199,19 +199,6 @@ impl Hemisphere {
                 remaining_instr
             };
 
-            // let call_stack = self.system.call_stack();
-            // if call_stack.0.len() > 0 {
-            //     tracing::debug!("({}) call stack:\n{call_stack}", self.system.cpu.pc);
-            // }
-
-            // tracing::debug!("xfb: {}", self.system.bus.video.top_xfb_address());
-            // tracing::debug!(
-            //     "refresh rate: {} resolution: {:?}",
-            //     self.system.bus.video.refresh_rate(),
-            //     self.system.bus.video.xfb_resolution(),
-            // );
-
-            // tracing::debug!("interrupts: {:?}", self.system.bus.video.regs.interrupts);
             let e = self.exec(Limits {
                 cycles: cycles_to_run,
                 instructions,
