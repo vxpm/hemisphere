daba9fbcb91c0fbd7519883e812e4805eb736e13
vinícius x <viniciusximenespm@gmail.com>
2025-10-16 22:22:37 -0300

    wesl eval

--- renderer/build.rs
+++ renderer/build.rs
@@ -2,10 +2,16 @@ use wesl::Wesl;
 
 fn main() {
     let mut wesl = Wesl::new("shaders");
-    wesl.use_imports(true);
     wesl.use_sourcemap(true);
-    wesl.use_lower(true);
-    wesl.use_stripping(false);
+    wesl.set_options(wesl::CompileOptions {
+        imports: true,
+        condcomp: false,
+        generics: false,
+        strip: true,
+        lower: true,
+        validate: true,
+        ..Default::default()
+    });
 
     wesl.build_artifact(&"package::blit".parse().unwrap(), "blit");
     wesl.build_artifact(&"package::uber".parse().unwrap(), "uber");
