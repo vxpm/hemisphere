0e2e997009f9acfeaeddf19ed20c902f44dca273
vinícius x <viniciusximenespm@gmail.com>
2025-11-08 14:09:37 -0300

    wip

--- cores/src/lib.rs
+++
@@ -1,5 +0,0 @@
-/// Returns a `Box<[T; LEN]>` filled with `elem`.
-#[inline(always)]
-pub fn boxed_array<T: Clone, const LEN: usize>(elem: T) -> Box<[T; LEN]> {
-    vec![elem; LEN].into_boxed_slice().try_into().ok().unwrap()
-}
