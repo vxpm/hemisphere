231c2e6bc85ddfb33722865e8ce61c3d39b670b8
vinícius x <viniciusximenespm@gmail.com>
2025-12-19 23:25:34 -0300

    codewarrior demangler

--- app-modules/src/debug.rs
+++ app-modules/src/debug.rs
@@ -6,6 +6,18 @@ use hemisphere::{
 use mapfile_parser::MapFile;
 use std::{borrow::Cow, path::Path};
 
+fn demangle(s: &str) -> String {
+    let cw_options = cwdemangle::DemangleOptions {
+        omit_empty_parameters: true,
+        mw_extensions: false,
+    };
+    if let Some(demangled) = cwdemangle::demangle(s, &cw_options) {
+        return demangled;
+    }
+
+    addr2line::demangle_auto(Cow::Borrowed(s), Some(gimli::DW_LANG_C_plus_plus)).into_owned()
+}
+
 pub struct Addr2LineDebug(addr2line::Loader);
 
 impl Addr2LineDebug {
@@ -16,10 +28,7 @@ impl Addr2LineDebug {
 
 impl DebugModule for Addr2LineDebug {
     fn find_symbol(&self, addr: Address) -> Option<String> {
-        self.0
-            .find_symbol(addr.value() as u64)
-            .map(|s| addr2line::demangle_auto(Cow::Borrowed(s), Some(gimli::DW_LANG_C_plus_plus)))
-            .map(|s| s.into_owned())
+        self.0.find_symbol(addr.value() as u64).map(demangle)
     }
 
     fn find_location(&self, addr: Address) -> Option<Location<'_>> {
@@ -48,7 +57,7 @@ impl DebugModule for MapFileDebug {
         self.0
             .find_symbol_by_vram(addr.0 as u64)
             .0
-            .map(|s| addr2line::demangle_auto(Cow::Borrowed(&s.symbol.name), None).into_owned())
+            .map(|s| demangle(&s.symbol.name))
     }
 
     fn find_location(&self, addr: Address) -> Option<Location<'_>> {
