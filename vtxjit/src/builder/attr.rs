396fff8fda773578490b55ccb46969ce2bbeb4ef
vinícius x <viniciusximenespm@gmail.com>
2026-01-01 21:44:36 -0300

    arrggggh

--- vtxjit/src/builder/attr.rs
+++ vtxjit/src/builder/attr.rs
@@ -1,3 +1,4 @@
+use crate::builder::{MEMFLAGS, ParserBuilder};
 use cranelift::{codegen::ir, prelude::InstBuilder};
 use hemisphere::system::gx::{
     Vertex,
@@ -10,18 +11,24 @@ use hemisphere::system::gx::{
 };
 use util::offset_of;
 
-use crate::builder::ParserBuilder;
-
-const MEMFLAGS: ir::MemFlags = ir::MemFlags::new().with_notrap().with_can_move();
-
 pub trait AttributeExt: Attribute {
     const ARRAY_OFFSET: usize;
 
+    fn set_default(_parser: &mut ParserBuilder) {}
     fn parse(desc: &Self::Descriptor, parser: &mut ParserBuilder) -> u32;
 }
 
 impl AttributeExt for attributes::PosMatrixIndex {
     const ARRAY_OFFSET: usize = usize::MAX;
+
+    fn set_default(parser: &mut ParserBuilder) {
+        parser.bd.ins().store(
+            MEMFLAGS,
+            parser.consts.default_pos,
+            parser.vars.vertex_ptr,
+            offset_of!(Vertex, position_matrix) as i32,
+        );
+    }
 
     fn parse(_: &Self::Descriptor, parser: &mut ParserBuilder) -> u32 {
         let index = parser
@@ -29,7 +36,8 @@ impl AttributeExt for attributes::PosMatrixIndex {
             .ins()
             .load(ir::types::I8, MEMFLAGS, parser.vars.data_ptr, 0);
 
-        let index = parser.get_matrix_idx(false, index);
+        parser.include_matrix(false, index);
+
         parser.bd.ins().store(
             MEMFLAGS,
             index,
@@ -64,7 +72,7 @@ impl AttributeExt for attributes::Position {
                     .ins()
                     .load($ty, MEMFLAGS, parser.vars.data_ptr, $offset);
 
-                let value = if $ty.is_float() {
+                let value = if $ty.lane_type().is_float() {
                     value
                 } else if signed {
                     parser
@@ -76,6 +84,15 @@ impl AttributeExt for attributes::Position {
                         .bd
                         .ins()
                         .fcvt_from_uint(ir::types::F32.by($ty.lane_count()).unwrap(), value)
+                };
+
+                let shift = if $ty.lane_count() == 1 {
+                    shift
+                } else {
+                    parser
+                        .bd
+                        .ins()
+                        .splat(ir::types::F32.by($ty.lane_count()).unwrap(), shift)
                 };
 
                 parser.bd.ins().fmul(value, shift)
@@ -112,6 +129,7 @@ fn read_rgba(format: ColorFormat, parser: &mut ParserBuilder) -> ir::Value {
     let to_float = |parser: &mut ParserBuilder, rgba, max: u8| {
         let rgba = parser.bd.ins().fcvt_from_uint(ir::types::F32X4, rgba);
         let recip = parser.bd.ins().f32const(1.0 / (max as f32));
+        let recip = parser.bd.ins().splat(ir::types::F32X4, recip);
         parser.bd.ins().fmul(rgba, recip)
     };
 
@@ -132,10 +150,16 @@ fn read_rgba(format: ColorFormat, parser: &mut ParserBuilder) -> ir::Value {
                 .load(ir::types::I8, MEMFLAGS, parser.vars.data_ptr, 2);
             let a = parser.bd.ins().iconst(ir::types::I8, 255);
 
-            let rgba = parser.bd.ins().scalar_to_vector(ir::types::I8X4, r);
+            let r = parser.bd.ins().uextend(ir::types::I32, r);
+            let g = parser.bd.ins().uextend(ir::types::I32, g);
+            let b = parser.bd.ins().uextend(ir::types::I32, b);
+            let a = parser.bd.ins().uextend(ir::types::I32, a);
+
+            let rgba = parser.bd.ins().scalar_to_vector(ir::types::I32X4, r);
             let rgba = parser.bd.ins().insertlane(rgba, g, 1);
             let rgba = parser.bd.ins().insertlane(rgba, b, 2);
             let rgba = parser.bd.ins().insertlane(rgba, a, 3);
+
             to_float(parser, rgba, 255)
         }
         ColorFormat::Rgb888x => todo!(),
@@ -146,6 +170,7 @@ fn read_rgba(format: ColorFormat, parser: &mut ParserBuilder) -> ir::Value {
                 .bd
                 .ins()
                 .load(ir::types::I8X4, MEMFLAGS, parser.vars.data_ptr, 0);
+
             to_float(parser, rgba, 255)
         }
         _ => panic!("reserved color format"),
